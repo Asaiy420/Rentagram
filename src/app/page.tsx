14e55bdd9f276688b52f8d65a4ab57bb0b49ecb9
Asaiy420 <kcaryan05@gmail.com>
2025-04-05 19:40:05 +0545

    added Navbar UI's

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,7 @@ import ModeToggle from "@/components/ModeToggle";
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>SignIn</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-      <Button variant={"secondary"}>Click me</Button>
+      <h1>HOME PAGE CONTENT </h1>
     </div>
   );
 }
